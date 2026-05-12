671c5799da67906ad4b415520acf5f6d80d19fb9
KexinChengWednesday <3297538022@qq.com>
2026-05-12 23:30:21 +1000

    fail test

--- src/test/java/task7_2_HD_s224384754/TodoServiceTest.java
+++ src/test/java/task7_2_HD_s224384754/TodoServiceTest.java
@@ -64,6 +64,6 @@ class TodoServiceTest {
     @Test
     void testIntentionallyFailing() {
         Todo todo = service.create("This will fail");
-        assertEquals(1L, todo.getId()); // real id is 1
+        assertEquals(999L, todo.getId()); // real id is 1
     }
 }
